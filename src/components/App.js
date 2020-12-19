fcde4f1b02cc1fe601936aa5f025660b1c7da2e8
Praveen Kumar <praveenscience@gmail.com>
2020-12-19 11:36:56 +0000

    Created handlers for both light bulbs.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { hasClickedCoords } from "../helpers/coords";
 
 export default class App extends Component {
   handleClick = e => {
     e.preventDefault();
     const { pageX, pageY } = e;
-    console.log({ pageX, pageY });
+    // Handler for Lightbulb 1
+    console.log(hasClickedCoords(pageX, pageY, [770, 300, 940, 425]) ? "Clicked on Light Bulb 1" : null);
+    // Handler for Lightbulb 2
+    console.log(hasClickedCoords(pageX, pageY, [1225, 375, 1320, 455]) ? "Clicked on Light Bulb 2" : null);
   };
   render() {
     return (
