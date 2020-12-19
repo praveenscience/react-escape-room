9fb8b30be07b7259923d1496705dd795ec910f0a
Praveen Kumar <praveenscience@gmail.com>
2020-12-19 11:30:17 +0000

    Added click event handler.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  handleClick = e => {
+    e.preventDefault();
+    const { pageX, pageY } = e;
+    console.log({ pageX, pageY });
+  };
   render() {
     return (
       <div className="App">
-        <div className="clickArea"></div>
+        <div className="clickArea" onClick={this.handleClick}></div>
       </div>
     );
   }
