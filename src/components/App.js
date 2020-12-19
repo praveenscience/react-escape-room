50546f9521d622db58808ce67a30932bf61cdf77
Praveen Kumar <praveenscience@gmail.com>
2020-12-19 11:42:02 +0000

    Created a state object and points.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import { hasClickedCoords } from "../helpers/coords";
 
 export default class App extends Component {
+  state = {
+    Bulb1: false,
+    Bulb2: false,
+    Points: 0
+  };
   handleClick = e => {
     e.preventDefault();
     const { pageX, pageY } = e;
     // Handler for Lightbulb 1
-    console.log(hasClickedCoords(pageX, pageY, [770, 300, 940, 425]) ? "Clicked on Light Bulb 1" : null);
+    if (!this.state.Bulb1 && hasClickedCoords(pageX, pageY, [770, 300, 940, 425]))
+      if (window.prompt("What do you see?") === "Light 1") {
+        this.setState({ Bulb1: true, Points: this.state.Points + 1 });
+        window.alert("Wow! That's right! You got a point!");
+      } else {
+        window.alert("Sorry! Try again!");
+      }
     // Handler for Lightbulb 2
-    console.log(hasClickedCoords(pageX, pageY, [1225, 375, 1320, 455]) ? "Clicked on Light Bulb 2" : null);
+    if (!this.state.Bulb2 && hasClickedCoords(pageX, pageY, [1225, 375, 1320, 455]))
+      if (window.prompt("What do you see?") === "Light 2") {
+        window.alert("Wow! That's right! You got a point!");
+        this.setState({ Bulb2: true, Points: this.state.Points + 1 });
+      } else {
+        window.alert("Sorry! Try again!");
+      }
   };
   render() {
     return (
